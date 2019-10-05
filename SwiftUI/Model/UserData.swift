1848bf950398182bfef21da1394bb644d3174b13
m-sugawara <masa.sugawar@gmail.com>
2019-10-05 18:22:39 +0900

    implement favorite button

---
+++ SwiftUI/Model/UserData.swift
@@ -0,0 +1,15 @@
+//
+//  UserData.swift
+//  SwiftUIFirst
+//
+//  Created by M_Sugawara on 2019/10/05.
+//  Copyright © 2019 Sugawar. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+}
